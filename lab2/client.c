095902a3b29d6585185d83c2c8b67f80a6241d96
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-03-02 01:10:45 +0100

    Add response read + refactor

--- lab2/client.c
+++ lab2/client.c
@@ -1,38 +1,58 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 #define RETRY_INTERVAL_MS 2000
+#define BUFFER_SIZE_BYTES 1024
+#define DATA_FILE_NAME "data.txt"
 
-char MESSAGE[256];
-
-void writeToFile(const char* fileName, const char text[], int characters)
+void waitForFile()
 {
   while (open("lockfile", O_CREAT | O_EXCL, 0) == -1)
   {
-    printf("Server is busy, please wait...\n");
+    printf("[CLIENT] Server is busy, please wait...\n");
     usleep(1000 * RETRY_INTERVAL_MS);
   }
+}
 
+void writeTextToFile(const char* fileName, char* text, int bytes)
+{
   int file = open(fileName, O_CREAT | O_WRONLY | O_APPEND);
-  write(file, text, characters);
+  write(file, text, bytes);
 
   // simulate traffic - hold lock a little longer
   usleep(1000 * RETRY_INTERVAL_MS * 5);
-
   unlink("lockfile");
+}
+
+char* readTextFromFile(const char* fileName)
+{
+    int file = open(fileName, O_RDONLY);
+    char* responseBuffer = malloc( BUFFER_SIZE_BYTES );
+    read(file, responseBuffer, BUFFER_SIZE_BYTES);
+    return responseBuffer;
 }
 
 char* getUserInput()
 {
-  printf("Type:\n");
-  scanf("%s", MESSAGE);
-  return MESSAGE;
+  char* messageBuffer = malloc( BUFFER_SIZE_BYTES );
+  scanf("%s", messageBuffer);
+  return messageBuffer;
 }
 
 void main(void)
 {
-  const char fileName[] = "data.txt";
-  const char* input = getUserInput();
+  printf("[CLIENT] Enter the message:\n");
+  char* input = getUserInput();
 
-  writeToFile(fileName, input, sizeof(input));
+  printf("[CLIENT] Waiting for server...\n");
+  waitForFile();
+
+  printf("[CLIENT] Sending to server...\n");
+  writeTextToFile(DATA_FILE_NAME, input, BUFFER_SIZE_BYTES);
+
+  printf("[CLIENT] Server response:\n");
+  char* response = readTextFromFile(DATA_FILE_NAME);
+  printf("%s\n", response);
 }
