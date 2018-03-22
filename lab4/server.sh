5de6c43bab2e6fb8ce81e3291f3b6868cbc514e5
mmajcher <majcher.marek@gmail.com>
2018-03-22 14:02:16 +0100

    MAKE 4 BETTER

--- lab4/server.sh
+++ lab4/server.sh
@@ -11,13 +11,29 @@ fi
 mkfifo $SERVER_FIFO
 chmod 777 $SERVER_FIFO
 
+
+function handler {
+    local CLIENT_FIFO=$1
+    local REQUEST_ARG=$2
+
+    echo "[HANDLER]: ARG == $REQUEST_ARG"
+    echo "[HANDLER]: ADR == $CLIENT_FIFO"
+
+    echo "[HANDLER]: sending response..."
+    echo $(($REQUEST_ARG*$REQUEST_ARG)) > $CLIENT_FIFO
+    echo "[HANDLER]: response sent!"
+}
+
 echo "[SERVER]: waiting for request..."
 while true; do
     read REQUEST < $SERVER_FIFO
     if [ -n "$REQUEST" ]; then
+        REQUEST_ARR=($REQUEST)
+        CLIENT_FIFO=${REQUEST_ARR[0]}
+        NUMBA=${REQUEST_ARR[1]}
         echo "[SERVER]: forked - [$REQUEST]"
-        ./handle_request.sh $REQUEST &
+        handler $CLIENT_FIFO $NUMBA &
     fi
-    sleep 3
+    sleep 3                     # WHY
 done
 exit 0
