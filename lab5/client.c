402b75112638413b7ad1b79c4b411407631ae5bb
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-10 00:40:30 +0200

    Read server response in client

--- lab5/client.c
+++ lab5/client.c
@@ -5,6 +5,7 @@
 #include <fcntl.h>
 
 #define CLIENT_FIFO "client_fifo"
+#define SERVER_FIFO "server_fifo"
 
 typedef struct request_t
 {
@@ -16,7 +17,14 @@ typedef struct request_t
 
 } request;
 
-void sendRequest(int fifoHandle, request* req)
+typedef struct response_t
+{
+    // payload
+    char* name;
+
+} response;
+
+void sendRequest(int serverFifoHandle, request* req)
 {
     int requestSize = sizeof(req->payloadSize) + req->payloadSize;
     void* buffer;
@@ -26,18 +34,45 @@ void sendRequest(int fifoHandle, request* req)
     memcpy(buffer + sizeof(req->payloadSize), &req->id, sizeof(req->id));
     memcpy(buffer + sizeof(req->payloadSize) + sizeof(req->id), req->homepath, req->payloadSize - sizeof(req->id));
 
-    write(fifoHandle, buffer, requestSize);
+    write(serverFifoHandle, buffer, requestSize);
     free(buffer);
+}
+
+response* receiveResponse(int clientFifoHandle, int responseSize)
+{
+    void* buffer = malloc(responseSize);
+    response* res = malloc(responseSize);
+    res->name = malloc(responseSize);
+
+    read(clientFifoHandle, buffer, responseSize);
+    memcpy(res->name, buffer, responseSize);
+
+    free(buffer);
+    return res;
+}
+
+void waitForResponse()
+{
+    int clientFifoHandle = open(CLIENT_FIFO, O_RDONLY);
+
+    int responseSize = 0;
+    if (read(clientFifoHandle, &responseSize, sizeof(int)) > 0)
+    {
+        printf("res size: %i \n", responseSize);
+        response* res = receiveResponse(clientFifoHandle, responseSize);
+        printf("%s \n", res->name);
+    }
 }
 
 int main(int argc, char * argv[])
 {
-    int clientFifoHandle = open(CLIENT_FIFO, O_WRONLY);
+    int serverFifoHandle = open(SERVER_FIFO, O_WRONLY);
 
     request req;
     req.id = atoi(argv[1]);
     req.homepath = getenv("HOME");
     req.payloadSize = sizeof(req.id) + strlen(req.homepath);
+    sendRequest(serverFifoHandle, &req);
 
-    sendRequest(clientFifoHandle, &req);
+    waitForResponse();
 }
