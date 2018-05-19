eaa3edb044df69b309c2a7da2a3bc29ff133ebe2
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-05-19 15:37:24 +0200

    Mark mouse click on boar

--- project/client.c
+++ project/client.c
@@ -8,6 +8,14 @@
 #define BOARD_X_MARGIN 20
 #define BOARD_Y_MARGIN 20
 
+#define FIELD_EMPTY 0
+#define FIELD_SHIP 1
+#define FIELD_HIT 2
+#define FIELD_MISS 3
+#define BOARD_NONE 0
+#define BOARD_PLAYER 1
+#define BOARD_ENEMY 2
+
 typedef struct coords_st {
   int board;
   int x;
@@ -24,8 +32,22 @@ XEvent event;
 coords_t get_cell_by_xy(int x, int y)
 {
   int board = 0;
-  int cell_x = (x - BOARD_X_MARGIN) / CELL_SIZE_PX;
-  int cell_y = (y - BOARD_Y_MARGIN) / CELL_SIZE_PX;
+  int cell_x = 0;
+  int cell_y = 0;
+
+  if (x >= BOARD_X_MARGIN && x <= BOARD_SIZE + BOARD_X_MARGIN)
+  {
+    board = BOARD_PLAYER;
+  }
+
+  if (x >= BOARD_SIZE + 2 * BOARD_X_MARGIN && x <= 2 * BOARD_SIZE + 2 * BOARD_X_MARGIN)
+  {
+    board = BOARD_ENEMY;
+    x -= (BOARD_SIZE + BOARD_X_MARGIN);
+  }
+
+  cell_x = (x - BOARD_X_MARGIN) / CELL_SIZE_PX;
+  cell_y = (y - BOARD_Y_MARGIN) / CELL_SIZE_PX;
   coords_t coords = { .board = board, .x = cell_x, .y = cell_y };
   return coords;
 }
@@ -41,28 +63,32 @@ void draw_board(int board[BOARD_SIZE][BOARD_SIZE])
   {
     // horizontal lines
     XDrawLine(display, window, gc, BOARD_X_MARGIN, BOARD_Y_MARGIN + i * CELL_SIZE_PX, BOARD_X_MARGIN + BOARD_SIZE_PX, BOARD_Y_MARGIN + i * CELL_SIZE_PX);
-  }
-
-
-  XAllocNamedColor(display, colormap, "blue", &color, &exact_color);
-  XSetForeground(display, gc, color.pixel);
-  for (int i = 0; i < BOARD_SIZE + 1; i++)
-  {
     // vertical lines
     XDrawLine(display, window, gc, BOARD_X_MARGIN + i * CELL_SIZE_PX, BOARD_Y_MARGIN, BOARD_X_MARGIN + i * CELL_SIZE_PX, BOARD_Y_MARGIN + BOARD_SIZE_PX);
   }
 
-  XAllocNamedColor(display, colormap, "gray", &color, &exact_color);
-  XSetForeground(display, gc, color.pixel);
   for (int i = 0; i < BOARD_SIZE; i++)
   {
     for (int j = 0; j < BOARD_SIZE; j++)
     {
-      if (board[i][j] == 1)
+      switch (board[i][j])
       {
-        // filled rectangles
-        XFillRectangle(display, window, gc, BOARD_X_MARGIN + i * CELL_SIZE_PX + 1, BOARD_Y_MARGIN + j * CELL_SIZE_PX + 1, CELL_SIZE_PX - 1, CELL_SIZE_PX - 1);
+        case FIELD_EMPTY:
+            XAllocNamedColor(display, colormap, "white", &color, &exact_color);
+            break;
+        case FIELD_SHIP:
+            XAllocNamedColor(display, colormap, "green", &color, &exact_color);
+            break;
+        case FIELD_MISS:
+            XAllocNamedColor(display, colormap, "gray", &color, &exact_color);
+            break;
+        case FIELD_HIT:
+            XAllocNamedColor(display, colormap, "red", &color, &exact_color);
+            break;
       }
+
+      XSetForeground(display, gc, color.pixel);
+      XFillRectangle(display, window, gc, BOARD_X_MARGIN + i * CELL_SIZE_PX + 1, BOARD_Y_MARGIN + j * CELL_SIZE_PX + 1, CELL_SIZE_PX - 1, CELL_SIZE_PX - 1);
     }
   }
 
@@ -89,7 +115,7 @@ int main()
     RootWindow(display, screen),
     0,
     0,
-    BOARD_SIZE_PX + 2 * BOARD_X_MARGIN,
+    2 * (BOARD_SIZE_PX + 2 * BOARD_X_MARGIN),
     BOARD_SIZE_PX + 2 * BOARD_Y_MARGIN,
     1,
     BlackPixel(display, screen),
@@ -115,10 +141,12 @@ int main()
     }
   }
 
-  /* add example filled fields */
-  board[2][3] = 1;
-  board[7][1] = 1;
-  board[6][8] = 1;
+  /* add some ships */
+  board[2][3] = FIELD_SHIP;
+  board[3][3] = FIELD_SHIP;
+  board[7][1] = FIELD_SHIP;
+  board[8][1] = FIELD_SHIP;
+  board[9][1] = FIELD_SHIP;
 
   /* event loop */
   while(1)
@@ -138,7 +166,17 @@ int main()
     {
       printf("mouse pressed X:%i Y:%i\n", event.xbutton.x, event.xbutton.y);
       coords_t selected_cell = get_cell_by_xy(event.xbutton.x, event.xbutton.y);
-      printf("cell X:%i Y:%i\n", selected_cell.x, selected_cell.y);
+      printf("cell B:%i X:%i Y:%i\n", selected_cell.board, selected_cell.x, selected_cell.y);
+
+      int field = board[selected_cell.x][selected_cell.y];
+      if (field == FIELD_EMPTY || field == FIELD_MISS)
+      {
+        board[selected_cell.x][selected_cell.y] = FIELD_MISS;
+      }
+      if (field == FIELD_SHIP || field == FIELD_HIT)
+      {
+        board[selected_cell.x][selected_cell.y] = FIELD_HIT;
+      }
     }
     /* print key pressed */
     if (event.type == KeyPress)
