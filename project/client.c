857b7228ad147051fc86ac346648c67ee4b5970f
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-05-19 15:03:28 +0200

    Get currenctly selected board cell by XY

--- project/client.c
+++ project/client.c
@@ -2,43 +2,57 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define BOARD_SIZE 10 // plansza ma wymiary BOARD_SIZE x BOARD_SIZE pól
-#define CELL_SIZE_PX 20 // rozmiar pola planszy w pikselach
-#define BOARD_X 0 // odległość między planszą a lewym brzegiem okna
-#define BOARD_Y 50 // odległość między planszą a górnym brzegiem okna
-#define BOTTOM_MARGIN 50 // odległość między planszą a dolnym brzegiem okna
-#define BOARD_SIZE_PX BOARD_SIZE * CELL_SIZE_PX // rozmiar planszy w pikselach
+#define BOARD_SIZE 10
+#define CELL_SIZE_PX 20
+#define BOARD_SIZE_PX BOARD_SIZE * CELL_SIZE_PX
+#define BOARD_X_MARGIN 20
+#define BOARD_Y_MARGIN 20
 
-// zmienne globalne Xlib
+typedef struct coords_st {
+  int board;
+  int x;
+  int y;
+} coords_t;
+
+// xlib global variables
 Display *display;
 Window window;
 int screen;
 GC gc;
 XEvent event;
 
+coords_t get_cell_by_xy(int x, int y)
+{
+  int board = 0;
+  int cell_x = (x - BOARD_X_MARGIN) / CELL_SIZE_PX;
+  int cell_y = (y - BOARD_Y_MARGIN) / CELL_SIZE_PX;
+  coords_t coords = { .board = board, .x = cell_x, .y = cell_y };
+  return coords;
+}
+
 void draw_board(int board[BOARD_SIZE][BOARD_SIZE])
 {
   Colormap colormap = DefaultColormap(display, screen);
   XColor color, exact_color;
 
-  XAllocNamedColor(display, colormap, "black", &color, &exact_color);
+  XAllocNamedColor(display, colormap, "blue", &color, &exact_color);
   XSetForeground(display, gc, color.pixel);
   for (int i = 0; i < BOARD_SIZE + 1; i++)
   {
-    // rysuje linie poziome
-    XDrawLine(display, window, gc, BOARD_X, BOARD_Y + i * CELL_SIZE_PX, BOARD_X + BOARD_SIZE_PX, BOARD_Y + i * CELL_SIZE_PX);
+    // horizontal lines
+    XDrawLine(display, window, gc, BOARD_X_MARGIN, BOARD_Y_MARGIN + i * CELL_SIZE_PX, BOARD_X_MARGIN + BOARD_SIZE_PX, BOARD_Y_MARGIN + i * CELL_SIZE_PX);
   }
 
-
-  XAllocNamedColor(display, colormap, "green", &color, &exact_color);
-  XSetForeground(display, gc, color.pixel);
-  for (int i = 0; i < BOARD_SIZE + 1; i++)
-  {
-    // rysuje linie pionowe
-    XDrawLine(display, window, gc, BOARD_X + i * CELL_SIZE_PX, BOARD_Y, BOARD_X + i * CELL_SIZE_PX, BOARD_Y + BOARD_SIZE_PX);
-  }
 
   XAllocNamedColor(display, colormap, "blue", &color, &exact_color);
+  XSetForeground(display, gc, color.pixel);
+  for (int i = 0; i < BOARD_SIZE + 1; i++)
+  {
+    // vertical lines
+    XDrawLine(display, window, gc, BOARD_X_MARGIN + i * CELL_SIZE_PX, BOARD_Y_MARGIN, BOARD_X_MARGIN + i * CELL_SIZE_PX, BOARD_Y_MARGIN + BOARD_SIZE_PX);
+  }
+
+  XAllocNamedColor(display, colormap, "gray", &color, &exact_color);
   XSetForeground(display, gc, color.pixel);
   for (int i = 0; i < BOARD_SIZE; i++)
   {
@@ -46,7 +60,8 @@ void draw_board(int board[BOARD_SIZE][BOARD_SIZE])
     {
       if (board[i][j] == 1)
       {
-        XFillRectangle(display, window, gc, BOARD_X + i * CELL_SIZE_PX, BOARD_Y + j * CELL_SIZE_PX, CELL_SIZE_PX, CELL_SIZE_PX);
+        // filled rectangles
+        XFillRectangle(display, window, gc, BOARD_X_MARGIN + i * CELL_SIZE_PX + 1, BOARD_Y_MARGIN + j * CELL_SIZE_PX + 1, CELL_SIZE_PX - 1, CELL_SIZE_PX - 1);
       }
     }
   }
@@ -69,7 +84,17 @@ int main()
   /* set GC */
   gc = DefaultGC(display, screen);
   /* create window */
-  window = XCreateSimpleWindow(display, RootWindow(display, screen), 10, 10, 300, 300, 1, BlackPixel(display, screen), WhitePixel(display, screen));
+  window = XCreateSimpleWindow(
+    display,
+    RootWindow(display, screen),
+    0,
+    0,
+    BOARD_SIZE_PX + 2 * BOARD_X_MARGIN,
+    BOARD_SIZE_PX + 2 * BOARD_Y_MARGIN,
+    1,
+    BlackPixel(display, screen),
+    WhitePixel(display, screen)
+  );
   /* Process Window Close Event through event handler so XNextEvent does Not fail */
   Atom delWindow = XInternAtom(display, "WM_DELETE_WINDOW", 0);
   XSetWMProtocols(display, window, &delWindow, 1);
@@ -79,7 +104,6 @@ int main()
   XSelectInput(display, window, ExposureMask | KeyPressMask | ButtonPressMask);
   /* map (show) the window */
   XMapWindow(display, window);
-
 
   /* create game board */
   int board[BOARD_SIZE][BOARD_SIZE];
@@ -91,9 +115,10 @@ int main()
     }
   }
 
-  /* add two taken points */
+  /* add example filled fields */
   board[2][3] = 1;
   board[7][1] = 1;
+  board[6][8] = 1;
 
   /* event loop */
   while(1)
@@ -106,12 +131,14 @@ int main()
     if(event.type == Expose)
     {
       //XFillRectangle(d, w, DefaultGC(d, s), 20, 20, 10, 10);
-      printf("expose\n");
+      printf("exposed\n");
     }
     /* print mouse click location */
     if(event.type == ButtonPress)
     {
-      printf("mouse X:%i Y:%i\n", event.xbutton.x, event.xbutton.y);
+      printf("mouse pressed X:%i Y:%i\n", event.xbutton.x, event.xbutton.y);
+      coords_t selected_cell = get_cell_by_xy(event.xbutton.x, event.xbutton.y);
+      printf("cell X:%i Y:%i\n", selected_cell.x, selected_cell.y);
     }
     /* print key pressed */
     if (event.type == KeyPress)
@@ -121,7 +148,7 @@ int main()
     /* handle windows close event */
     if(event.type == ClientMessage)
     {
-      printf("window close\n");
+      printf("window closed\n");
       break;
     }
   }
